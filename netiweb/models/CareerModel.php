e8de5714433684e5f37236df3667fd8e72795a2b
theary.rin <rin.theary@gmail.com>
2015-10-26 22:28:22 +0700

    Update

--- netiweb/models/CareerModel.php
+++ netiweb/models/CareerModel.php
@@ -1,20 +1,53 @@
 <?php
 
-/**
- * The CareeModel will contain all data functions for career page processing.
- *
- * @author:
- * @email:
- * @phone:
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-class CareerModel extends CI_Model {
-    // invoke parent's constructor
-    public function __construct() {
+
+/**
+ * Description of CareerModel
+ *
+ * @author Vongkol
+ */
+class CareerModel extends CI_Model{
+    
+     public function __construct() {
         parent::__construct();
     }
-    // default action
-    public function index()
-    {
+    private $t_career = "career";
+
+    /*
+     * This function is for getting data from table career to show at view
+     * Author: Theary RIN
+     */
+    public function get_career($id=null){
         
+        $this->db->select("*");
+        
+        if($id !=NULL){
+            $this->db->where("{$this->t_career}.careerid",$id);
+        }
+        return $this->db->get($this->t_career);
     }
+    /*
+     * This function is for doing edit career
+     * Author: Theary RIN
+     */
+    public function do_edit_career($title,$des,$career_id){
+        
+        if ($career_id != null) {
+            $this->db->where("{$this->t_career}.careerid", $career_id);
+        }
+
+        $data_edit = array(
+            "title" => $title,
+            "description" => $des
+        );
+
+        $this->db->update($this->t_career, $data_edit);
+        return $this;
+    }
+    
 }
