022944c1b66630f8b1a1e0724ec5f3d8156a03f9
theary.rin <rin.theary@gmail.com>
2015-10-27 21:47:22 +0700

    Add and Update Controller

--- netiweb/controllers/Customer.php
+++ netiweb/controllers/Customer.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class Partner extends CI_Controller {
+
+class Customer extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('partnermodel');
+        $this->load->model('customermodel');
     }
 
     //Default function
@@ -19,48 +20,33 @@ class Partner extends CI_Controller {
         if ($this->session->userid == false) {
             redirect(base_url('admin/login'));
         }
-        $data['title'] = "Partner List";
-        $data['list_partner'] = $this->partnermodel->get_partner();
+        $data['title'] = "Customer List";
+        $data['list_customer'] = $this->customermodel->get_customer();
         $this->load->view('master/header', $data);
-        $this->load->view('master/partner_list', $data);
+        $this->load->view('master/customer_list', $data);
         $this->load->view('master/footer');
     }
-
-    /*
-     * This function is for add new partner
-     * Author: Theary RIN
-     */
-
-    public function newpartner() {
-
-        if ($this->session->userid == false) {
+    public function newcustomer(){
+          if ($this->session->userid == false) {
             redirect(base_url('admin/login'));
         }
-        $data['title'] = "Add New Partner";
+        $data['title'] = "Add New Customer";
         $data["sms"] = "";
         $this->load->view('master/header', $data);
-        $this->load->view('master/partner_add',$data);
+        $this->load->view('master/customer_add',$data);
         $this->load->view('master/footer');
-        
     }
-
-    /*
-     * This function is for add new partner
-     * Author: Theary RIN
-     */
-
-    public function do_new_partner() {
-        
-        $data['title'] = "Add New Partner";
+    public function do_new_customer(){
+         $data['title'] = "Add New Customer";
         $data["sms"] =""; 
         // set the filter image types
-        $config['upload_path'] = 'assets/images/partner/';
+        $config['upload_path'] = 'assets/images/customer/';
         $config['allowed_types'] = 'gif|jpg|png|jpeg';
         $this->load->library("upload", $config);
-        $partner_name =  $this->input->post("partnername");
-        $check = $this->partnermodel->check_partner($partner_name);
+        $customer_name =  $this->input->post("customername");
+        $check = $this->customermodel->check_customer($customer_name);
         if($check){
-            $data["sms"] ="<span class='text-danger'>This partner has already.</span>";
+            $data["sms"] ="<span class='text-danger'>This customer has already.</span>";
         }else{
             if ($this->upload->do_upload("image")) {
                 $upload_data = $this->upload->data();
@@ -73,31 +59,30 @@ class Partner extends CI_Controller {
             }
             
             $data_insert = array(
-                "partnername" => $this->input->post("partnername"),
+                "customername" => $this->input->post("customername"),
                 "partimg" => $part,
                 "img" => $file_name,
-                "url" => $this->input->post("partnerurl")
+                "url" => $this->input->post("customerurl")
             );
 
-           $insert =  $this->partnermodel->add_new_partner($data_insert);
+           $insert =  $this->customermodel->add_new_customer($data_insert);
            if($insert){
                $data["sms"] = "<span class='text-info'>Data has been saved.</span>";
            }else{
-               $data["sms"] = "<span class='text-danger'>Cannot add new partner. Please check your data again</span>";
+               $data["sms"] = "<span class='text-danger'>Cannot add new customer. Please check your data again</span>";
            }
         }
         $this->load->view('master/header', $data);
-        $this->load->view('master/partner_add',$data);
+        $this->load->view('master/customer_add',$data);
         $this->load->view('master/footer');
     }
     /*
-     * This function is for deleting data partner
+     * This function is for delete customer
      * Author: Theary RIN
      */
-    public function delete_partner($partner_id){
+    public function delete_customer($customer_id){
         
-        $get_partner = $this->partnermodel->delete_partner($partner_id);
-        redirect("partner");
+        $get_customer = $this->customermodel->delete_customer($customer_id);
+        redirect("customer");
     }
-
 }
